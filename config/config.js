93ea3c6a4857e95a1660d87b223b01b599b1aaed
Aniket Roy <aniketroy10100@gmail.com>
2025-05-16 02:32:54 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/auth_system',
+    mongoURI: 'mongodb+srv://aniketroy:multiplication_server@cluster0.wd5jkso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     jwtSecret: '0bea859d8c39f5a954d6fa57dca1ded0c07a845e345a6c58e57e18f17634d6fa67a83ae74ab66b270b36591fd91c198e7bf094067c99933d1e3c88effe353764',
     jwtExpire: '24h',
     resetPasswordExpire: 3600000 // 1 hour
